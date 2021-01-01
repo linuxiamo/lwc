5806ef85da8c26785e046835225561d084265e7c
Umberto <31143638+linuxiamo@users.noreply.github.com>
2021-01-02 00:47:42 +0100

    Second commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 // next.config.js
 module.exports = {
+/*
     i18n: {
       locales: ['en-US', 'it-IT'],
       defaultLocale: 'it-IT',
     },
+*/
     target: 'serverless',
     webpack: function (config) {
       config.module.rules.push({
